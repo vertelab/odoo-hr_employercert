f19124b285f2b8bd24c699f38a9197ae68fd9629
HaojunZou <apollo_zhj@msn.com>
2015-03-23 16:46:15 +0100

    version 1.0

--- hr_employercert/__openerp__.py
+++ hr_employercert/__openerp__.py
@@ -15,14 +15,13 @@ Manage your employee.
     """,
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
-    'depends': ['hr', 'hr_attendance', 'hr_timesheet', 'hr_contract', 'hr_payroll', 'hr_holidays', 'mail'],
+    'depends': ['hr', 'hr_attendance', 'hr_timesheet', 'hr_contract', 'hr_payroll', 'hr_holidays', 'mail', 'report'],
     'data': [
         'hr_employercert_view.xml',
         'hr_employercert_report.xml',
         'views/report_employeereport.xml',
         'views/hr_employercert.xml',
        ],
-    #'demo': [''],
     'installable': True,
 }
 
